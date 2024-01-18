1384d85e0278161322f66a60a661f321aac1815a
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-01-18 20:47:50 +0700

    update submodules

--- worker/build.ps1
+++ worker/build.ps1
@@ -1,9 +1,12 @@
 cd daemon/hub
-go build -o ../package/hub.exe cmd/main.go
+go build -o ../../package/hub.exe cmd/main.go
 cd ..
-go build -o ./package/daemon.exe cmd/main.go
+go build -o ../package/daemon.exe cmd/main.go
 cd ..
 cd sunshine/build
 ninja
 cd ../../
-copy sunshine/build/libsunshine.dll daemon/package/libsunshine.dll
+del package/libsunshine.dll
+del package/test.exe
+copy sunshine/build/libsunshine.dll ./package/libsunshine.dll
+copy sunshine/build/test.exe ./package/test.exe
