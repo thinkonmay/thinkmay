d50e1c0e0e17b177eb5331a122123875db2ffe30
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-03-31 10:36:00 -0700

    update module

--- build.ps1
+++ build.ps1
@@ -20,9 +20,15 @@ cd app/moonlight
 .\scripts\build-arch.bat release x64
 cd ../..
 
+cd app/demo
+npm i
+npm run tauri build -- --debug
+cd ../..
+
 cd browser/win11
 npm i
 npm run tauri build
 cd ../..
 
 copy ./browser/win11/src-tauri/target/release/thinkmay.exe ./binary/thinkmay.exe
+copy ./app/demo/src-tauri/target/release/moonlight.exe ./binary/moonlight.exe
