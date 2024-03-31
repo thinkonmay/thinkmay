0c2abee73b2c784ab251b5086293890727d01664
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-03-31 08:42:20 -0700

    update submodules

--- build.ps1
+++ build.ps1
@@ -1,5 +1,19 @@
 cd worker
-./build.ps1
+
+cd webrtc
+go build -o ../../binary/hub.exe ./cmd/
+cd ..
+
+cd daemon
+go build -o ../../binary/daemon.exe ./service/window/
+cd ..
+
+cd sunshine/build
+ninja
+cd ../../
+del ../binary/libsunshine.dll
+copy sunshine/build/libsunshine.dll ../binary/libsunshine.dll
+
 cd ..
 
 cd app/moonlight
@@ -11,4 +25,4 @@ npm i
 npm run tauri build
 cd ../..
 
-copy ./browser/win11/src-tauri/target/release/thinkmay.exe ./worker/package/thinkmay.exe
+copy ./browser/win11/src-tauri/target/release/thinkmay.exe ./binary/thinkmay.exe
