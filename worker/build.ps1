0f0ac05ea328e6dba934470dd2f71e36080473cf
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2025-02-09 14:44:18 +0000

    update build

---
+++ worker/build.ps1
@@ -0,0 +1,3 @@
+cd webrtc; go build -o ..\..\assets\proxy.exe .\cmd\; cd ..
+cd daemon; go build -o ..\..\assets\daemon.exe .\cmd\; cd ..
+cd sunshine/build; ninja; cp sunshine.exe ..\..\..\assets\shmsunshine.exe; cd ../..
