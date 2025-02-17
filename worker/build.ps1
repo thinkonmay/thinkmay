6dd10611bd848aa6b64aab01225ce1b1c926cfd9
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2025-02-17 04:49:07 +0000

    add pb

--- worker/build.ps1
+++ worker/build.ps1
@@ -1,3 +1,4 @@
 cd webrtc; go build -o ..\..\assets\proxy.exe .\cmd\; cd ..
 cd daemon; go build -o ..\..\assets\daemon.exe .\cmd\; cd ..
+cd daemon; go build -o ..\..\assets\pb.exe .\pocketbase\cmd\; cd ..
 cd sunshine/build; ninja; cp sunshine.exe ..\..\..\assets\shmsunshine.exe; cd ../..
