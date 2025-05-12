ec9263dba5f5efd7e56ad6d78c605839458f817e
pigeatgarlic <huyhoangdo0205@gmail.com>
2025-05-13 00:10:35 +0700

    fix microphone

--- worker/build.ps1
+++ worker/build.ps1
@@ -1,5 +1,6 @@
 cd webrtc; go build -o ..\..\assets\proxy.exe .\cmd\; cd ..
 cd daemon; go build -o ..\..\assets\daemon.exe .\cmd\; cd ..
 cd daemon; go build -o ..\..\assets\pb.exe .\pocketbase\cmd\; cd ..
-cd steam; dotnet publish --sc true -o ../../assets/store /p:PublishSingleFile=true . ; cd ..
-cd sunshine/build; ninja; cp sunshine.exe ..\..\..\assets\shmsunshine.exe; cd ../..
+cd daemon; go build -o ..\..\assets\microphone.exe .\microphone\cmd\; cd ..
+#cd steam; dotnet publish --sc true -o ../../assets/store /p:PublishSingleFile=true . ; cd ..
+#cd sunshine/build; ninja; cp sunshine.exe ..\..\..\assets\shmsunshine.exe; cd ../..
