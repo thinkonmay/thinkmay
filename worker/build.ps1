a8cc125ff8b4291674a739ab0448f07cbf46e85c
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2025-05-14 09:21:24 +0700

    cli mode

--- worker/build.ps1
+++ worker/build.ps1
@@ -2,5 +2,5 @@ cd webrtc; go build -o ..\..\assets\proxy.exe .\cmd\; cd ..
 cd daemon; go build -o ..\..\assets\daemon.exe .\cmd\; cd ..
 cd daemon; go build -o ..\..\assets\pb.exe .\pocketbase\cmd\; cd ..
 cd daemon; go build -o ..\..\assets\microphone.exe .\microphone\cmd\; cd ..
-#cd steam; dotnet publish --sc true -o ../../assets/store /p:PublishSingleFile=true . ; cd ..
-#cd sunshine/build; ninja; cp sunshine.exe ..\..\..\assets\shmsunshine.exe; cd ../..
+cd steam; dotnet publish --sc true -o ../../assets/store /p:PublishSingleFile=true . ; cd ..
+cd sunshine/build; ninja; cp sunshine.exe ..\..\..\assets\shmsunshine.exe; cd ../..
