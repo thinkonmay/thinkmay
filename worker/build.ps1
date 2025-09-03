d532fabd1024bc4702de3625a537d6bf3a2e75ea
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2025-09-03 11:38:45 +0700

    update sunshine CI

--- worker/build.ps1
+++ worker/build.ps1
@@ -1,6 +1,2 @@
-cd webrtc; go build -o ..\..\assets\proxy.exe .\cmd\; cd ..
 cd daemon; go build -o ..\..\assets\daemon.exe .\cmd\; cd ..
-cd daemon; go build -o ..\..\assets\pb.exe .\pocketbase\cmd\; cd ..
-cd daemon; go build -o ..\..\assets\microphone.exe .\microphone\cmd\; cd ..
-cd steam; dotnet publish --sc true -o ../../assets/store /p:PublishSingleFile=true . ; cd ..
 cd sunshine/build; ninja; cp sunshine.exe ..\..\..\assets\shmsunshine.exe; cd ../..
