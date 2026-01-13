0cecbfbc511326ab3352c1286dd447eb2a4bbe0c
pigeatgarlic <huyhoangdo0205@gmail.com>
2026-01-13 04:50:37 +0000

    cancel grpc

---
+++ worker/build.sh
@@ -0,0 +1,5 @@
+cd daemon && go build -o ~/assets/daemon ./cmd && go build -o ~/assets/pb ./cmd/pocketbase && cd ..
+cd proxy && go build -o ~/assets/proxy ./cmd && cd ..
+sudo systemctl restart proxy
+sudo systemctl restart virtdaemon
+tail -f ~/assets/thinkmay.log
