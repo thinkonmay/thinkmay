f77c3377a11997bb4b9b2b100b9ca0b2e7707068
pigeatgarlic <huyhoangdo0205@gmail.com>
2026-02-03 10:03:05 +0000

    sort gpu

--- worker/build.sh
+++ worker/build.sh
@@ -7,6 +7,10 @@ cd proxy
 go build -o ~/assets/proxy ./cmd 
 cd ..
 
+scp ~/assets/proxy 192.168.1.8:~/assets/proxy.
+scp ~/assets/pb 192.168.1.8:~/assets/pb.
+scp ~/assets/daemon 192.168.1.8:~/assets/daemon.
+
 sudo systemctl restart proxy
 sudo systemctl restart virtdaemon
 tail -f ~/assets/thinkmay.log
